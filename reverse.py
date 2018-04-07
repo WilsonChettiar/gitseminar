ca5db62a3602dbd9df23d4bbbba75832b6b6cf0a
Wilson Chettiar <wilsonc@orionglobal.local>
2018-04-07 15:24:00 +0530

    update

--- reverse.py
+++ reverse.py
@@ -1,7 +1,6 @@
 #First comment for this code
-# Kachra comment mat daal
 
-a=str(input("Enter a string: "))
+b=str(input("Enter a string: "))
 print("Reverse of the string is: ")
-print(a[::-1])
+print(b[::-1])
 # a=10
